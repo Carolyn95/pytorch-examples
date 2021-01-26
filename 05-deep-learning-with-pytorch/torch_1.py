72075e26c14eaf30dbf74e7d3dd5bf8811d1761f
Carolyn CHEN <yanchongcarolynchen@gmail.com>
2021-01-26 10:14:57 +0800

    start dlwpt

---
+++ 05-deep-learning-with-pytorch/torch_1.py
@@ -0,0 +1,6 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F import torch.optim as optim
+# Variables named with a _t suffix are tensors stored in CPU memory, _g are tensors in GPU memory, and _a are NumPy arrays.
